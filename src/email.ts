c12c851e52b9dd4000e63a7c44797a9aaf599b68
Code Ape <34467684+mojocn@users.noreply.github.com>
2024-12-24 07:52:32 +0000

    Update temp email retrieval to check expiration timestamp

--- src/email.ts
+++ src/email.ts
@@ -5,9 +5,10 @@ import { TempEmail } from "./types";
 
 
 export async function mailHandler(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext): Promise<void> {
-    const {DB:db} = env;
-    const q = `SELECT * FROM temp_emails WHERE email=?`
-    const tempEmail = await db.prepare(q).bind(message.to).first<TempEmail>();
+    const { DB: db } = env;
+    const q = `SELECT * FROM temp_emails WHERE email=? AND expire_ts > ?`;
+    const nowTs = Math.floor(Date.now() / 1000);
+    const tempEmail = await db.prepare(q).bind(message.to, nowTs).first<TempEmail>();
     if (!tempEmail) {
         console.info(`No temp email found for ${message.to}`);
         message.setReject("Address not allowed");
