3d5acc6f481ca339ea34d75a85196d4e86e72b55
mojocn <neochau@gmail.com>
2025-06-11 21:21:45 +0800

    feat: implement apiNode for node registration and update apiNodes to use authentication middleware

--- src/app-nodes.ts
+++ src/app-nodes.ts
@@ -1,18 +1,12 @@
 import { Hono } from 'hono';
-import { Node, User } from './types';
+import { Node } from './types';
+import { mwAuthFn } from './mw-auth';
 
 
-// export const apiNodes = new Hono<{ Bindings: Env }>().use(mwAuthFn(true));
-export const apiNodes = new Hono<{ Bindings: Env }>();
+export const apiNodes = new Hono<{ Bindings: Env }>().use(mwAuthFn(true));
 
 
-interface AppStat {
-	traffic: { [key: string]: number };//uuid -> KB number
-	hostname: string;
-	sub_addresses: string[];
-	goroutine: number;
-	version_info: string,
-}
+
 
 
 apiNodes.get('/', async (c) => {
@@ -26,47 +20,7 @@ apiNodes.get('/', async (c) => {
 });
 
 
-apiNodes.post('/', async (c) => {
-	const body = await c.req.json<AppStat>();
-	const db = c.env.DB;
-	const nowTs = Math.floor(Date.now() / 1000);
-	const clientIP = c.req.header('cf-connecting-ip') || '';
-	const sub_addresses = body.sub_addresses.join(',');
-	await db.prepare('DELETE FROM nodes WHERE ip = ?').bind(clientIP).run();
-	const qq = 'INSERT INTO nodes (hostname, ip, active_ts, goroutine, version_info, sub_addresses) VALUES (?, ?, ?, ?, ?, ?)';
-	await db.prepare(qq).bind(body.hostname, clientIP, nowTs, body.goroutine, body.version_info, sub_addresses).run();
 
-	const qqq = 'SELECT * FROM users WHERE expire_ts > ? AND available_kb > ?';
-	const { results } = await db.prepare(qqq).bind(nowTs, 0).all<User>();
-
-	const allowUsers: { [key: string]: number } = {};
-	const stmtList: D1PreparedStatement[] = [];
-	const nowDate = new Date().toISOString().slice(0, 10);
-	for (const u of results) {
-		const id = u.id;
-		let usedKB = body.traffic[id] || 0;
-		if (usedKB < 0) {
-			usedKB = 0;
-		}
-		u.available_kb = u.available_kb - usedKB;
-		if (u.available_kb < 0) {
-			u.available_kb = 0;
-		} else {
-			allowUsers[id] = u.available_kb;
-		}
-		if (usedKB < 1) continue;
-		// console.log("set uid",id,u.available_kb)
-		const userKbUpdate = db.prepare('UPDATE users SET available_kb = ? WHERE id = ?').bind(u.available_kb, id);
-		const stmtUsageInsert = db.prepare('INSERT INTO usages (uid,kb,created_date,category) VALUES (?,?,?,?)').bind(id, usedKB, nowDate, 'raw');
-
-		stmtList.push(userKbUpdate);
-		stmtList.push(stmtUsageInsert);
-	}
-	if (stmtList.length > 0) {
-		await db.batch(stmtList);
-	}
-	return c.json(allowUsers);
-});
 
 
 apiNodes.delete('/', async (c) => {
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,24 @@
-import { Hono } from 'hono'
-import { apiUsers } from './app-users'
-import { apiNodes } from './app-nodes'
-import { apiUsages } from './app-usages'
-import { apiMeta } from './app-meta'
-import { apiOAuth } from './app-oauth'
-import { apiEmails } from './app-email'
-import { apiTelegram } from './app-telegram'
+import { Hono } from 'hono';
+import { apiUsers } from './app-users';
+import { apiNodes } from './app-nodes';
+import { apiUsages } from './app-usages';
+import { apiMeta } from './app-meta';
+import { apiOAuth } from './app-oauth';
+import { apiTelegram } from './app-telegram';
 import { apiAuth } from './app-auth';
 import { apiUser } from './app-user';
+import { apiNode } from './app-node';
 
 
+export const app = new Hono<{ Bindings: Env, Variables: Variables }>();
 
-export const app = new Hono<{ Bindings: Env, Variables: Variables }>()
+app.route('/api/node', apiNode);//for unchain-node register
+app.route('/api/user', apiUser);
+app.route('/api/meta', apiMeta);
 
-
-app.route('/api/user', apiUser)
-app.route('/api/users', apiUsers)
-app.route('/api/nodes', apiNodes)
-app.route('/api/usages', apiUsages)
-app.route('/api/meta', apiMeta)
-app.route("/api/auth", apiAuth)
-app.route("/api/oauth", apiOAuth)
-app.route("/api/emails", apiEmails)
-app.route("/api/telegram",apiTelegram)
+app.route('/api/users', apiUsers);//admin
+app.route('/api/nodes', apiNodes);//admin
+app.route('/api/usages', apiUsages);//admin
+app.route('/api/auth', apiAuth);
+app.route('/api/oauth', apiOAuth);
+app.route('/api/telegram', apiTelegram);
